a880e35bb4fdc6e14aaf210182665d18d33d0865
Katya Islyaeva <katyaislyaeva@MacBook-Pro-Katya.local>
2022-04-06 12:58:35 +0300

    Update project

---
+++ index.js
@@ -0,0 +1,6 @@
+//Модальное окно должно открываться по нажатию кнопки «Редактировать»,
+//а закрываться — при клике по крестику в правом верхнем  углу
+//метод addEventListener
+
+const buttonEditProfile = document.querySelector('.profile__edit-button');//кнопка редактирования
+  console.log(ghbdtn);
